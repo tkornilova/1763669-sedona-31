bf3d6a1a51cb6880d5fd35870700a1626313a32b
tkornilova <kornilova.tat07@gmail.com>
2021-07-11 11:18:39 -0600

    Исправления перед 2 проверкой

--- js/script.js
+++ js/script.js
@@ -2,18 +2,7 @@ const link = document.querySelector(".button-find-hotel");
 const popup = document.querySelector(".modal");
 const dateIn = popup.querySelector(".modal-date-in");
 const adults = popup.querySelector(".modal-adults");
-const children = popup.querySelector(".modal-children");
 const form = popup.querySelector(".booking-date");
-const storage = localStorage.getItem ("adults");
-
-let isStorageSupport = true;
-let storage = "";
-
-try {
-  storage = localStorage.getItem("adults");
-} catch (err) {
-  isStorageSupport = false;
-}
 
 /* Modal Show/Hide */
 
@@ -21,9 +10,6 @@ link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
   dateIn.focus();
-  if (storage) {
-    adults.value = storage;
-  }
 });
 
 /* Form Check */
@@ -31,11 +17,7 @@ link.addEventListener("click", function (evt) {
 form.addEventListener("submit", function (evt) {
   if (!dateIn.value || !dateOut.value || !adults.value) {
    evt.preventDefault();
-   console.log("Заполните все необходимые поля");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("adults", adults.value);
-    }
   }
 });
+
 
